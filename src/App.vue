5959a9f76a9dea4e138d1d2c9f5c4983e8d90472
Vaibhav Joshi <mailvaibhavjoshi@gmail.com>
2022-07-04 11:33:15 +0530

    Complete Drag and Drop vue components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <dropable class="my-dropable" @onDrop="dropHandler">
+      <draggable
+        @startDrag="startDragHandler"
+        @endDrag="endDragHandler"
+        :payload="{ testKey: 'testVal' }"
+        class="my-draggable"
+        class-on-drag="is-dragging"
+      />
+    </dropable>
+    <dropable class="my-dropable" @onDrop="dropHandler"> </dropable>
+    <dropable class="my-dropable" @onDrop="dropHandler">
+      <draggable
+        @startDrag="startDragHandler"
+        @endDrag="endDragHandler"
+        :payload="{ testKey: 'testVal' }"
+        class="my-draggable"
+        class-on-drag="is-dragging"
+      />
+    </dropable>
+    <dropable class="my-dropable" @onDrop="dropHandler"> </dropable>
+    <dropable class="my-dropable" @onDrop="dropHandler">
+      <draggable
+        @startDrag="startDragHandler"
+        @endDrag="endDragHandler"
+        :payload="{ testKey: 'testVal' }"
+        class="my-draggable"
+        class-on-drag="is-dragging"
+      />
+    </dropable>
+    <dropable class="my-dropable" @onDrop="dropHandler"> </dropable>
+    <dropable class="my-dropable" @onDrop="dropHandler">
+      <draggable
+        @startDrag="startDragHandler"
+        @endDrag="endDragHandler"
+        :payload="{ testKey: 'testVal' }"
+        class="my-draggable"
+        class-on-drag="is-dragging"
+      />
+    </dropable>
+    <dropable class="my-dropable" @onDrop="dropHandler"> </dropable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from "./components/Draggable.vue";
+import dropable from "./components/Dropable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    draggable,
+    dropable,
+  },
+  methods: {
+    startDragHandler(payload) {
+      console.log("Start drag payload:", payload);
+    },
+    endDragHandler(payload) {
+      console.log("End drag payload:", payload);
+    },
+    dropHandler(payload) {
+      console.log("Drop handler payload:", payload);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.my-dropable {
+  width: calc(25vw - 10px);
+  height: calc(50vh - 10px);
+  display: flex;
+  border: 1px solid red;
+  flex-direction: row;
+}
+
+.my-draggable {
+  margin: 10px;
+  width: 100%;
+  border: 1px solid brown;
+}
+
+.app {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.is-dragging {
+  border-width: 10px;
 }
 </style>
